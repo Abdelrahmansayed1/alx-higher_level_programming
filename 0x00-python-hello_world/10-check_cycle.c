fc77699a145eb1a20c169864723264fb3754217e
Abdelrahman Sayed <119951252+Abdelrahmansayed1@users.noreply.github.com>
2023-09-04 17:01:27 +0300

    Create 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list 
+ * Return: 1 if the list has a cycle, 0 
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *one = list;
+	listint_t *two = list;
+
+	if (!list)
+		return (0);
+
+	while (one && two && two->next)
+	{
+		one = two->next;
+		two = two->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
