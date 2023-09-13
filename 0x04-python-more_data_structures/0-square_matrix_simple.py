6ed766b802b04c875a968898cd0fb117de9c0ada
Abdelrahmansayed1 <abdelrahmansayedu@gmail.com>
2023-09-13 16:16:26 +0300

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_list)
-
-
+def square_matrix_simple(matrix=[]):
+        new_list = matrix.copy()
+        for i in range(len(new_list)):
+            new_list = map( lambda x: x**2,matrix(i))
+        return(new_list)
     
