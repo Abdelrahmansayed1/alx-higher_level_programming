5ad9a128727125e80ccdeead5cc3d5c09a82bc73
Abdelrahmansayed1 <abdelrahmansayedu@gmail.com>
2023-09-13 20:24:49 +0300

    osj

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    num = 0
+    de = 0
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        de += tup[1]
+
+    return (num / de)
