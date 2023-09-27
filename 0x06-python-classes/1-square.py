ffee2c962299f555e5cc40cf0e3049a82e5bc297
Abdelrahman Sayed <119951252+Abdelrahmansayed1@users.noreply.github.com>
2023-09-27 12:21:02 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 
 class Square:
+    """square."""
     def __init__(self, size):
+        """Initialize a new Square.
 
         Args:
+        size (int): The size of square.
+        """
         self.__size = size
